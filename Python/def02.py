470adcf3ab086d5e51d2d5f959c63efe648dde9e
SEIM <iramsekelli@gmail.com>
2022-09-05 22:42:20 +0900

    python def

---
+++ Python/def02.py
@@ -0,0 +1,10 @@
+def menu(entree='beef', drink='wine', dessert='ice'):
+    print('entree =', entree)
+    print('drink =', drink)
+    print('desset =', dessert)
+
+
+menu(entree='beef', drink='beer', dessert='ice')
+menu()
+
+
---
+++ Python/enumerate.py
@@ -0,0 +1,7 @@
+i = 0
+for fruite in ['apple', 'banana', 'orange']:
+    print(i, fruite)
+    i += 1
+print('-------------------')
+for i, fruite in enumerate(['apple', 'banana', 'orange']):
+    print(i, fruite)
